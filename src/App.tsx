9182b2bfe9bf0b7be6032d4a9bd62b8d471302bf
David <david.diez@tokiota.com>
2020-05-24 18:37:20 +0200

    Changed page structure
    Removed create react app default page sctructure in order to give the initial view a header and a sidebar menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app-container">
+      <header className="header">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          React + Typescript template
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="body">
+        <div className="sidebar open-menu">
+          Sidebar
+        </div>
+        <div className="content open-menu">
+          Content
+        </div>
+      </div>
     </div>
   );
 }
